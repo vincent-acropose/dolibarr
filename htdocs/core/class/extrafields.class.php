7ebad4f806159e13d394607b0c4ee85c82d821ec
Florian Henry <florian.henry@open-concept.pro>
2013-12-16 13:49:00 +0100

    reveiw extrafeilds

--- htdocs/core/class/extrafields.class.php
+++ htdocs/core/class/extrafields.class.php
@@ -725,8 +725,8 @@ class ExtraFields
 						while ($i < $num)
 						{
 							$obj = $this->db->fetch_object($resql);
-							$labeltoshow=dol_trunc($obj->$InfoFieldList[1],18);
-													if ($value==$obj->rowid)
+							$labeltoshow=dol_trunc($obj->$InfoFieldList[1],50);
+							if ($value==$obj->rowid)
 							{
 								$out.='<option value="'.$obj->rowid.'" selected="selected">'.$labeltoshow.'</option>';
 							}
