dadc7645e20703924824663ad127294e914a4ade
Florian Henry <florian.henry@open-concept.pro>
2014-03-31 15:46:06 +0200

    Change-Id: Id5f7cdba990384f8c52264fd61f4edc14c29a76d

--- htdocs/compta/journal/purchasesjournal_frais.php
+++ htdocs/compta/journal/purchasesjournal_frais.php
@@ -99,7 +99,7 @@ else
 	$sql .= " AND f.type IN (0,1,2,3)";
 if ($date_start && $date_end)
 	$sql .= " AND f.datef >= '" . $db->idate ( $date_start ) . "' AND f.datef <= '" . $db->idate ( $date_end ) . "'";
-$sql .= " AND s.rowid IN (SELECT fk_societe FROM " . MAIN_DB_PREFIX . "categorie_fournisseur WHERE fk_categorie IN (61))";
+$sql .= " AND s.rowid IN (SELECT fk_societe FROM " . MAIN_DB_PREFIX . "categorie_fournisseur WHERE fk_categorie IN (62))";
 $sql .= " ORDER BY s.nom";
 
 dol_syslog ( "sql=" . $sql );
