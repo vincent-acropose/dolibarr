d5e43159f089498484379a7787999ff0186ea301
root <root@protegral.yziact.net>
2014-04-16 11:02:25 +0200

    Modif locale

--- htdocs/fourn/commande/dispatch.php
+++ htdocs/fourn/commande/dispatch.php
@@ -85,6 +85,20 @@ if ($_POST["action"] ==	'dispatch' && $user->rights->fournisseur->commande->rece
 		}
 	}
 
+
+	if (! $notrigger)
+	{
+		global $conf, $langs, $user;
+		// Appel des triggers
+		include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+		$interface=new Interfaces($db);
+		$result_trigger=$interface->run_triggers('ORDER_SUPPLIER_DISPATCH',$commande,$user,$langs,$conf);
+		if ($result_trigger < 0) { $error++; $commande->errors=$interface->errors; }
+		// Fin appel triggers
+
+		$db->commit();
+	}
+
 	if ($result > 0)
 	{
 		header("Location: dispatch.php?id=".$_GET["id"]);
