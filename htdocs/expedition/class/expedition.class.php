ead38985cdd30b3dbc73c7d21da5c048d74dcf64
Serveur ATM <alexis@atm-consulting.fr>
2014-09-03 17:05:53 +0200

    modification spécifique

--- htdocs/expedition/class/expedition.class.php
+++ htdocs/expedition/class/expedition.class.php
@@ -548,7 +548,7 @@ class Expedition extends CommonObject
 					$mouvS = new MouvementStock($this->db);
 					// We decrement stock of product (and sub-products)
 					// We use warehouse selected for each line
-					$result=$mouvS->livraison($user, $obj->fk_product, $obj->fk_entrepot, $obj->qty, $obj->subprice, $langs->trans("ShipmentValidatedInDolibarr",$numref));
+					$result=$mouvS->livraison($user, $obj->fk_product, $obj->fk_entrepot, $obj->qty, $obj->subprice, $langs->trans("ShipmentValidatedInDolibarr",$this->getNomUrl(1),$soc->getNomUrl(1)));
 					if ($result < 0) { $error++; break; }
 				}
 			}
