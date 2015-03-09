fced6027c867bdd51487353a199916ce70ef8958
Alexis Algoud <alexis@atm-consulting.fr>
2015-03-09 14:43:54 +0100

    local merdier

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -1875,6 +1875,7 @@ class Facture extends CommonInvoice
 	            // Call trigger
 	            $result=$this->call_trigger('BILL_PAYED',$user);
 	            if ($result < 0) $error++;
+               
 	            // End call triggers
 			}
 			else
--- htdocs/core/class/interfaces.class.php
+++ htdocs/core/class/interfaces.class.php
@@ -245,8 +245,7 @@ class Interfaces
         {
             $dir=dol_buildpath($reldir,0);
             $newdir=dol_osencode($dir);
-            //print "xx".$dir;exit;
-
+            
             // Check if directory exists (we do not use dol_is_dir to avoid loading files.lib.php at each call)
             if (! is_dir($newdir)) continue;
 
@@ -255,6 +254,7 @@ class Interfaces
             {
                 while (($file = readdir($handle))!==false)
                 {
+                    
                     if (is_readable($newdir.'/'.$file) && preg_match('/^interface_([0-9]+)_([^_]+)_(.+)\.class\.php/',$file,$reg))
                     {
 						$part1=$reg[1];
@@ -262,7 +262,6 @@ class Interfaces
 						$part3=$reg[3];
 
                         $modName = 'Interface'.ucfirst($reg[3]);
-                        //print "file=$file"; print "modName=$modName"; exit;
                         if (in_array($modName,$modules))
                         {
                             $langs->load("errors");
