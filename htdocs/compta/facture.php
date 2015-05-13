0d1b1ea8cd6a79bd5a71e039b6330168b06daa45
Alexis Algoud <alexis@atm-consulting.fr>
2015-05-13 18:05:26 +0200

    FIX send mail, copy sendto don't read the list of contact
    Conflicts:
    	htdocs/comm/propal.php
    	htdocs/compta/facture.php

--- htdocs/compta/facture.php
+++ htdocs/compta/facture.php
@@ -1568,7 +1568,8 @@ if (($action == 'send' || $action == 'relance') && ! $_POST['addfile'] && ! $_PO
 			$from = $_POST['fromname'] . ' <' . $_POST['frommail'] . '>';
 			$replyto = $_POST['replytoname'] . ' <' . $_POST['replytomail'] . '>';
 			$message = $_POST['message'];
-			$sendtocc = $_POST['sendtocc'];
+			$receivercc = GETPOST('receivercc');
+			$sendtocc = ($receivercc!=='') ? $receivercc : $_POST ['sendtocc'];
 			$deliveryreceipt = $_POST['deliveryreceipt'];
 
 			if ($action == 'send') {
