d77f60d9f6ffcc60205bde98fd98aa5a90b69c12
phf <phf@atm-consulting.fr>
2015-05-25 11:26:37 +0200

    FIX : requete en erreur

--- htdocs/contact/list.php
+++ htdocs/contact/list.php
@@ -143,7 +143,7 @@ if ((!$user->rights->societe->contact->lire && !$socid) || $search_sale) $sql .=
 $sql.= " FROM ".MAIN_DB_PREFIX."socpeople as p";
 $sql.= " LEFT JOIN ".MAIN_DB_PREFIX."c_country as co ON co.rowid = p.fk_pays";
 $sql.= " LEFT JOIN ".MAIN_DB_PREFIX."societe as s ON s.rowid = p.fk_soc";
-if ((!$user->rights->societe->contact->lire && !$socid) || $search_sale) $sql.= ", ".MAIN_DB_PREFIX."socpeople_sales_representatives as ssr"; // We need this table joined to the select in order to filter by sale
+if ((!$user->rights->societe->contact->lire && !$socid) || $search_sale) $sql.= " LEFT JOIN ".MAIN_DB_PREFIX."socpeople_sales_representatives as ssr ON p.rowid = ssr.fk_socpeople"; // We need this table joined to the select in order to filter by sale
 if (! empty($search_categ)) $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX."categorie_contact as cs ON p.rowid = cs.fk_socpeople"; // We need this table joined to the select in order to filter by categ
 if (!$user->rights->societe->client->voir && !$socid) $sql .= " LEFT JOIN ".MAIN_DB_PREFIX."societe_commerciaux as sc ON s.rowid = sc.fk_soc";
 $sql.= ' WHERE p.entity IN ('.getEntity('societe', 1).')';
@@ -269,6 +269,7 @@ else
 
 //print $sql;
 dol_syslog("contact/list.php", LOG_DEBUG);
+
 $result = $db->query($sql);
 if ($result)
 {
