2f3e2542cf211b53358b9123f07e588e6e586d73
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-06-19 17:44:10 +0200

    New : ajout d'un lien sur l'écriture bancaire vers le bon de prélèvement

--- htdocs/compta/prelevement/class/bonprelevement.class.php
+++ htdocs/compta/prelevement/class/bonprelevement.class.php
@@ -461,6 +461,7 @@ class BonPrelevement extends CommonObject
                         $paiement->amounts      = $amounts;
                         $paiement->paiementid   = 3; //
                         $paiement->num_paiement = $this->ref ;
+						$paiement->id_prelevement = $this->id ;
 						
                         $paiement_id = $paiement->create($user);
 						
