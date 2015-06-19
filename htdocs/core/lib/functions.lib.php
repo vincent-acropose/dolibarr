7eb187c08e63e7848b8cb6b55a01aa2f3f3bc15d
maxime peyrot <maxime.peyrot@atm-consulting.fr>
2015-06-19 12:09:49 +0200

    FIX : Correction entité html utilisé pour les guillemets

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -471,7 +471,7 @@ function dol_escape_htmltag($stringtoescape,$keepb=0)
 	// escape quotes and backslashes, newlines, etc.
 	$tmp=dol_html_entity_decode($stringtoescape,ENT_COMPAT,'UTF-8');
 	if ($keepb) $tmp=strtr($tmp, array("\r"=>'\\r',"\n"=>'\\n'));
-	else $tmp=strtr($tmp, array("\r"=>'\\r',"\n"=>'\\n',"<b>"=>'','</b>'=>'','"'=>'&quote;'));
+	else $tmp=strtr($tmp, array("\r"=>'\\r',"\n"=>'\\n',"<b>"=>'','</b>'=>'','"'=>'&quot;'));
 	return dol_htmlentities($tmp,ENT_COMPAT,'UTF-8');
 }
 
