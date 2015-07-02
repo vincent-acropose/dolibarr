3360f59066263ff7039927ae4e1357a2d09d86b5
Gauthier <gauthier.verdol@atm-consulting.fr>
2015-07-02 16:35:29 +0200

    NEW : Ajout trigger livraison commande fournisseur

--- htdocs/fourn/class/fournisseur.commande.class.php
+++ htdocs/fourn/class/fournisseur.commande.class.php
@@ -1564,7 +1564,7 @@ class CommandeFournisseur extends CommonOrder
      *	@param	string	$comment	Comment
      *	@return	int					<0 if KO, >0 if OK
      */
-    function Livraison($user, $date, $type, $comment)
+    function Livraison($user, $date, $type, $comment, $notrigger=false)
     {
         $result = 0;
 
@@ -1589,7 +1589,19 @@ class CommandeFournisseur extends CommonOrder
                 dol_syslog(get_class($this)."::Livraison", LOG_DEBUG);
                 $resql=$this->db->query($sql);
                 if ($resql)
-                {
+                {	
+		        	if(! $notrigger) {
+		                global $conf, $langs, $user;
+						// Call trigger
+						$result=$this->call_trigger('ORDERSUPPLIER_ADD_LIVRAISON',$user);
+						if ($result < 0)
+		                {
+		                    $this->db->rollback();
+		                    return -1;
+		                }
+						// End call triggers
+		            }
+					
                     $result = 0;
                     $result=$this->log($user, $statut, $date, $comment);
 
