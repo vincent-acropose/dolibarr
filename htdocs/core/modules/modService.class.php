000ec2b8f3d3708571ffc75a13b6208095ba3ba2
maxime peyrot <maxime.peyrot@atm-consulting.fr>
2015-07-23 17:38:56 +0200

    FIX : Services => Activités

--- htdocs/core/modules/modService.class.php
+++ htdocs/core/modules/modService.class.php
@@ -50,7 +50,7 @@ class modService extends DolibarrModules
 		$this->family = "products";
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
-		$this->description = "Gestion des services";
+		$this->description = "Gestion des activités";
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 		$this->version = 'dolibarr';
