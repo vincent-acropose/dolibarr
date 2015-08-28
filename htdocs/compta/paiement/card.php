7899742dffa1ddbf3c8ee4b9a8c2bf789fe4fbaf
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-08-28 16:39:47 +0200

    ADD : hook sur paiement

--- htdocs/compta/paiement/card.php
+++ htdocs/compta/paiement/card.php
@@ -47,6 +47,8 @@ if ($user->societe_id) $socid=$user->societe_id;
 
 $object = new Paiement($db);
 
+$hookmanager = new HookManager($db);
+$hookmanager->initHooks(array('viewpaiementcard'));
 
 /*
  * Actions
@@ -265,6 +267,9 @@ if (! empty($conf->banque->enabled))
     }
 }
 
+$parameters=array();
+$reshook=$hookmanager->executeHooks('formObjectOptions',$parameters,$object,$action); // Note that $action and $object may have been modified by hook
+
 print '</table>';
 
 
@@ -304,7 +309,7 @@ if ($resql)
 		{
 			$objp = $db->fetch_object($resql);
 			$var=!$var;
-			print '<tr '.$bc[$var].'>';
+			print '<tr id="row-'.$objp->facid.'" '.$bc[$var].'>';
 
             $invoice=new Facture($db);
             $invoice->fetch($objp->facid);
@@ -345,6 +350,9 @@ if ($resql)
 			}
 			$total = $total + $objp->amount;
 			$i++;
+			
+			$parameters=array();
+			$reshook=$hookmanager->executeHooks('printObjectLine',$parameters,$objp,$action); // Note that $action and $object may have been modified by hook
 		}
 	}
 	$var=!$var;
