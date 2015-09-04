c77b18cd9c3751000027e1868c3f7491b30937a0
Maxime Kohlhaas <maxime@atm-consulting.fr>
2015-09-04 16:16:30 +0200

    Fix : extrafield CF => SI

--- htdocs/fourn/facture/fiche.php
+++ htdocs/fourn/facture/fiche.php
@@ -1235,7 +1235,7 @@ if ($action == 'create')
             $datedue=($datetmp==''?-1:$datetmp);
 			
 			// Replicate extrafields
-			$objectsrc->fetch_optionals($originid);
+			$objectsrc->fetch_optionals(GETPOST('originid'));
 			$object->array_options = $objectsrc->array_options;
         }
     }
