60bd20a8ea7d2f873f914f5c5bc4eb767c0b0b50
Serveur ATM <alexis@atm-consulting.fr>
2016-01-07 17:59:44 +0100

    FIX : remise replanish

--- htdocs/product/stock/replenish.php
+++ htdocs/product/stock/replenish.php
@@ -102,7 +102,7 @@ if ($action == 'order' && isset($_POST['valid']))
                 $qty = GETPOST('tobuy'.$i, 'int');
                 $desc = GETPOST('desc'.$i, 'alpha');
                 $sql = 'SELECT fk_product, fk_soc, ref_fourn';
-                $sql .= ', tva_tx, unitprice FROM ';
+                $sql .= ', tva_tx, unitprice, remise_percent FROM ';
                 $sql .= MAIN_DB_PREFIX . 'product_fournisseur_price';
                 $sql .= ' WHERE rowid = ' . $supplierpriceid;
                 $resql = $db->query($sql);
@@ -123,6 +123,7 @@ if ($action == 'order' && isset($_POST['valid']))
 	                    $line->total_tva = $line->total_ht * $tva;
 	                    $line->total_ttc = $line->total_ht + $line->total_tva;
 	                    $line->ref_fourn = $obj->ref_fourn;
+			    $line->remise_percent = $obj->remise_percent;
 	                    $suppliers[$obj->fk_soc]['lines'][] = $line;
                 	}
                 }
