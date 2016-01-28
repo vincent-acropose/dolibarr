c3fa4bb65992e8cf3cf83e500832597a04ed7a2f
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2016-01-28 11:35:21 +0100

    ADD : possibilité de moficier les descriptions de ligne sur facture de situation

--- htdocs/core/tpl/objectline_edit.tpl.php
+++ htdocs/core/tpl/objectline_edit.tpl.php
@@ -101,7 +101,7 @@ $coldisplay=-1; // We remove first td
 		$doleditor=new DolEditor('product_desc',$line->description,'',164,$toolbarname,'',false,true,$enable,$nbrows,'98%');
 		$doleditor->Create();
 	} else {
-		print '<textarea id="desc" class="flat" name="desc" readonly style="width: 200px; height:80px;">' . $line->description . '</textarea>';
+		print '<textarea id="desc" class="flat" name="desc" style="width: 200px; height:80px;">' . $line->description . '</textarea>';
 	}
 	?>
 	</td>
