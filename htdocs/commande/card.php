169a6828c3ffd08160a4637acf667d67229f390c
gauthier <gauthier.verdol@atm-consulting.fr>
2016-09-02 16:16:54 +0200

    NEW : sur création d'expédition, on saute l'étape de sélection d'entrepôt globale

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -2350,7 +2350,7 @@ if ($action == 'create' && $user->rights->commande->creer)
 					if ($object->statut > Commande::STATUS_DRAFT && $object->statut < Commande::STATUS_CLOSED && $object->getNbOfProductsLines() > 0) {
 						if (($conf->expedition_bon->enabled && $user->rights->expedition->creer) || ($conf->livraison_bon->enabled && $user->rights->expedition->livraison->creer)) {
 							if ($user->rights->expedition->creer) {
-								print '<div class="inline-block divButAction"><a class="butAction" href="' . DOL_URL_ROOT . '/expedition/shipment.php?id=' . $object->id . '">' . $langs->trans('ShipProduct') . '</a></div>';
+								print '<div class="inline-block divButAction"><a class="butAction" href="' . DOL_URL_ROOT . '/expedition/card.php?action=create&origin=commande&origin_id=' . $object->id . '">' . $langs->trans('ShipProduct') . '</a></div>';
 							} else {
 								print '<div class="inline-block divButAction"><a class="butActionRefused" href="#" title="' . dol_escape_htmltag($langs->trans("NotAllowed")) . '">' . $langs->trans('ShipProduct') . '</a></div>';
 							}
