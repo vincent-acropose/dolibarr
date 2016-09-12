43bc214840edd4d6a954d64a191228c5c8c4fd05
gauthier <gauthier.verdol@atm-consulting.fr>
2016-09-12 15:42:24 +0200

    NEW : "backport" autocomplete selectWarehouses() (copier coller de la fonction car sur plusieurs commits)

--- htdocs/fourn/commande/dispatch.php
+++ htdocs/fourn/commande/dispatch.php
@@ -30,6 +30,7 @@
 require '../../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/core/modules/supplier_order/modules_commandefournisseur.php';
 require_once DOL_DOCUMENT_ROOT.'/product/stock/class/entrepot.class.php';
+require_once DOL_DOCUMENT_ROOT.'/product/class/html.formproduct.class.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/fourn.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.commande.class.php';
 require_once DOL_DOCUMENT_ROOT.'/fourn/class/fournisseur.commande.dispatch.class.php';
@@ -246,6 +247,7 @@ if ($action == 'dispatch' && $user->rights->fournisseur->commande->receptionner)
  */
 
 $form =	new Form($db);
+$form_product =	new FormProduct($db);
 $warehouse_static = new Entrepot($db);
 $supplierorderdispatch = new CommandeFournisseurDispatch($db);
 
@@ -548,11 +550,13 @@ if ($id > 0 || ! empty($ref))
 							
 							if (count($listwarehouses)>1)
 							{
-								print $form->selectarray("entrepot".$suffix, $listwarehouses, $ent, 1, 0, 0, '', 0, 0, $disabled);
+								//print $form->selectarray("entrepot".$suffix, $listwarehouses, $ent, 1, 0, 0, '', 0, 0, $disabled);
+								print $form_product->selectWarehouses($ent, "entrepot".$suffix, '', 1);
 							}
 							elseif  (count($listwarehouses)==1)
 							{
-								print $form->selectarray("entrepot".$suffix, $listwarehouses, $ent, 0, 0, 0, '', 0, 0, $disabled);
+								//print $form->selectarray("entrepot".$suffix, $listwarehouses, $ent, 0, 0, 0, '', 0, 0, $disabled);
+								print $form_product->selectWarehouses($ent, "entrepot".$suffix);
 							}
 							else
 							{
--- htdocs/product/class/html.formproduct.class.php
+++ htdocs/product/class/html.formproduct.class.php
@@ -101,23 +101,43 @@ class FormProduct
 	 *
 	 *  @param	int		$selected       Id of preselected warehouse ('' for no value, 'ifone'=select value if one value otherwise no value)
 	 *  @param  string	$htmlname       Name of html select html
-	 *  @param  string	$filtertype     For filter
+	 *  @param  string	$filtertype     For filter, additional filter on status other then 1
 	 *  @param  int		$empty			1=Can be empty, 0 if not
 	 * 	@param	int		$disabled		1=Select is disabled
 	 * 	@param	int		$fk_product		Add quantity of stock in label for product with id fk_product. Nothing if 0.
 	 *  @param	string	$empty_label	Empty label if needed (only if $empty=1)
+	 *  @param	int		$showstock		1=show stock count
+	 *  @param	int		$forcecombo		force combo iso ajax select2
+	 *  @param	array	$events			events to add to select2
+	 *  @param  string  $morecss        Add more css classes
 	 * 	@return	string					HTML select
 	 */
-	function selectWarehouses($selected='',$htmlname='idwarehouse',$filtertype='',$empty=0,$disabled=0,$fk_product=0,$empty_label='')
+	 /**
+	  * 
+	  * FONCTION RECUPEREE DE DOLIBARR 4.0 POUR L'AUTOCOMPLETE
+	  * (AVEC UN COPIER COLLER CAR TROP DE COMMIT)
+	  * 
+	  */
+	function selectWarehouses($selected='',$htmlname='idwarehouse',$filtertype='',$empty=0,$disabled=0,$fk_product=0,$empty_label='', $showstock=0, $forcecombo=0, $events=array(), $morecss='')
 	{
-		global $langs,$user;
+		global $conf,$langs,$user;
 
-		dol_syslog(get_class($this)."::selectWarehouses $selected, $htmlname, $filtertype, $empty, $disabled, $fk_product",LOG_DEBUG);
-
-		$this->loadWarehouses($fk_product);
+		dol_syslog(get_class($this)."::selectWarehouses $selected, $htmlname, $filtertype, $empty, $disabled, $fk_product, $empty_label, $showstock, $forcecombo, $morecss",LOG_DEBUG);
+		
+		$out='';
+		
+		$this->loadWarehouses($fk_product, '', + $filtertype); // filter on numeric status
 		$nbofwarehouses=count($this->cache_warehouses);
 
-		$out='<select class="flat"'.($disabled?' disabled':'').' id="'.$htmlname.'" name="'.($htmlname.($disabled?'_disabled':'')).'">';
+		if ($conf->use_javascript_ajax && ! $forcecombo)
+		{
+			include_once DOL_DOCUMENT_ROOT . '/core/lib/ajax.lib.php';
+			$comboenhancement = ajax_combobox($htmlname, $events);
+			$out.= $comboenhancement;
+			$nodatarole=($comboenhancement?' data-role="none"':'');
+		}
+		
+		$out.='<select class="flat'.($morecss?' '.$morecss:'').'"'.($disabled?' disabled':'').' id="'.$htmlname.'" name="'.($htmlname.($disabled?'_disabled':'')).'"'.$nodatarole.'>';
 		if ($empty) $out.='<option value="-1">'.($empty_label?$empty_label:'&nbsp;').'</option>';
 		foreach($this->cache_warehouses as $id => $arraytypes)
 		{
@@ -125,7 +145,7 @@ class FormProduct
 			if ($selected == $id || ($selected == 'ifone' && $nbofwarehouses == 1)) $out.=' selected';
 			$out.='>';
 			$out.=$arraytypes['label'];
-			if ($fk_product) $out.=' ('.$langs->trans("Stock").': '.($arraytypes['stock']>0?$arraytypes['stock']:'?').')';
+			if (($fk_product || ($showstock > 0)) && ($arraytypes['stock'] != 0 || ($showstock > 0))) $out.=' ('.$langs->trans("Stock").':'.$arraytypes['stock'].')';
 			$out.='</option>';
 		}
 		$out.='</select>';
