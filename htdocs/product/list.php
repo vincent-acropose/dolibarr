e707dfc3c92c3e4c4d9bc246c8f31a297b1c2d58
florian HENRY <florian.henry@atm-consulting.fr>
2017-06-29 10:12:55 +0200

    fix product list with filter extrafields

--- htdocs/product/list.php
+++ htdocs/product/list.php
@@ -319,15 +319,15 @@ else
 	// Add where from extra fields
 	foreach ($search_array_options as $key => $val)
 	{
-	    $crit=$val;
-	    $tmpkey=preg_replace('/search_options_/','',$key);
-	    $typ=$extrafields->attribute_type[$tmpkey];
-	    $mode=0;
-	    if (in_array($typ, array('int','double'))) $mode=1;    // Search on a numeric
-	    if ($val && ( ($crit != '' && ! in_array($typ, array('select'))) || ! empty($crit)))
-	    {
-	        $sql .= natural_search('ef.'.$tmpkey, $crit, $mode);
-	    }
+		$crit=$val;
+		$tmpkey=preg_replace('/search_options_/','',$key);
+		$typ=$extrafields->attribute_type[$tmpkey];
+		$mode=0;
+		if (in_array($typ, array('int','double'))) $mode=1;    // Search on a numeric
+		if ($val && ( ($crit != '' && ! in_array($typ, array('select'))) || ! empty($crit)))
+		{
+			$sql .= natural_search('ef.'.$tmpkey, $crit, $mode);
+		}
 	}
 	// Add where from hooks
 	$parameters=array();
@@ -593,7 +593,26 @@ else
 			{
 			   foreach($extrafields->attribute_label as $key => $val)
 			   {
-					if (! empty($arrayfields["ef.".$key]['checked'])) print '<td class="liste_titre"></td>';
+					if (! empty($arrayfields["ef.".$key]['checked'])) {
+						$align=$extrafields->getAlignFlag($key);
+						$typeofextrafield=$extrafields->attribute_type[$key];
+						print '<td class="liste_titre'.($align?' '.$align:'').'">';
+						if (in_array($typeofextrafield, array('varchar', 'int', 'double', 'select')) && empty($extrafields->attribute_computed[$key]))
+						{
+							$crit=$val;
+							$tmpkey=preg_replace('/search_options_/','',$key);
+							$searchclass='';
+							if (in_array($typeofextrafield, array('varchar', 'select'))) $searchclass='searchstring';
+							if (in_array($typeofextrafield, array('int', 'double'))) $searchclass='searchnum';
+							print '<input class="flat'.($searchclass?' '.$searchclass:'').'" size="4" type="text" name="search_options_'.$tmpkey.'" value="'.dol_escape_htmltag($search_array_options['search_options_'.$tmpkey]).'">';
+						}
+						else
+						{
+							// for the type as 'checkbox', 'chkbxlst', 'sellist' we should use code instead of id (example: I declare a 'chkbxlst' to have a link with dictionnairy, I have to extend it with the 'code' instead 'rowid')
+							echo $extrafields->showInputField($key, $search_array_options['search_options_'.$key], '', '', 'search_');
+						}
+						print '</td>';
+					}
 			   }
 			}
     		// Fields from hook
